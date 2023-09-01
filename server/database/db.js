5c9e63cdb724c66c219f444f0c9db35012526391
gopal <gopal221014@gmail.com>
2023-09-01 08:35:09 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = () => {
+
+    const MONGODB_URI = "mongodb+srv://user1:123@cluster0.eqmes7e.mongodb.net/"
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
